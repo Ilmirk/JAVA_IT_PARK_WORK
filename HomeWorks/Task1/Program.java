74a9924ba8657650a4ae92ecd9fb672eec89f7f7
Ilmir <ilmir_kashapov@mail.ru>
2017-03-06 20:31:41 +0300

    add HomeWorks

---
+++ HomeWorks/Task1/Program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Program {
+	public static void main(String [] args){
+		Scanner scanner = new Scanner(System.in);
+
+		int a = 1;
+		int b = scanner.nextInt();
+
+		for (int b = 2 || b == 3) {
+			if (a % i == 0) {
+				
+			}
+		}
+	}
+}
---
+++ HomeWorks/Task2/Program.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Program {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int i = 1;
+        int c = 0;
+
+        while (a != -1) {
+
+            if (i % 2 == 0) {
+                c += a;
+            }
+
+            a = scanner.nextInt();
+            i++;
+        }
+
+        System.out.println (c);
+    }
+}
