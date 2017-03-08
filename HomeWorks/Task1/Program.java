4f54bf84d43bc82d6a500c7c62b7dd9631235765
Ilmir <ilmir_kashapov@mail.ru>
2017-03-08 20:58:02 +0300

    add HomeWork

--- HomeWorks/Task1/Program.java
+++ HomeWorks/Task1/Program.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
 class Program {
-	public static void main(String [] args){
-		Scanner scanner = new Scanner(System.in);
-
-		int a = 1;
-		int b = scanner.nextInt();
-
-		for (int b = 2 || b == 3) {
-			if (a % i == 0) {
-				
-			}
-		}
-	}
+    public static void main(String[] args) {
+        int a;
+        int i = 2;
+        int c = 1;
+        Scanner scanner = new Scanner(System.in);
+        a = scanner.nextInt();
+        while (a != -1) {
+            while ((i*i <= a)&&(a % i != 0)) {
+                i++;
+            }
+            if (i*i > a) {
+                c = c * a;
+            }
+            a = scanner.nextInt();
+        }
+        System.out.println (c);
+    }
 }
