ae1d8b2ab3641eb013b68a1149426f8d63bbfbfc
Ilmir <ilmir_kashapov@mail.ru>
2017-03-11 14:15:06 +0300

    add Task5

---
+++ HomeWorks/Task5/Program.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+class Program {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println ("Введите количество элементов массива:");
+        int arrays = scanner.nextInt();
+
+        int arr[] = new int[arrays];
+        int sum = 0;
+
+        System.out.println("Введите элементы массива:");
+
+        for (int i = 0; i < arr.length ; i++) {
+
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.println("Сумма элементов массива = " + sum);
+    }
+}
