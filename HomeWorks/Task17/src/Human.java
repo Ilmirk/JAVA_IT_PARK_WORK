aa6b6def1ad125bbe285039ccfc6bcfd6286c376
Ilmir <ilmir_kashapov@mail.ru>
2017-03-29 21:11:43 +0300

    add Task17

---
+++ HomeWorks/Task17/src/Human.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Ilmir on 28.03.2017.
+ */
+public class Human {
+
+    private String name;
+    private int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ HomeWorks/Task17/src/Program.java
@@ -0,0 +1,42 @@
+/**
+ * Created by Ilmir on 28.03.2017.
+ */
+
+// Создать массив людей, сортировать пузырьком по возрасту
+public class Program {
+    public static void main(String[] args) {
+
+        Human humans[] = new Human[] {
+                new Human("Иван",53),
+                new Human("Ильмир",27),
+                new Human("Гульназ",42),
+                new Human("Андрей",30),
+                new Human("Юрий",18),
+                new Human("Стас",10),
+                new Human("Елена",74),
+                new Human("Альберт",14),
+                new Human("Костя",17),
+                new Human("Марсель", 22) };
+
+        for (int i = 0; i < humans.length; i++){
+            System.out.println("Имя  = \t" + humans[i].getName() + "\t Возраст = " + humans[i].getAge());
+        }
+
+        Human temp;
+        for(int i = humans.length - 1; i > 0; i--) {
+            for(int j = 0; j < i; j++) {
+                if(humans[j].getAge() > humans[j + 1].getAge()) {
+                    temp = humans[j];
+                    humans[j] = humans[j + 1];
+                    humans[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Имя \t Возраст");
+        System.out.println("----------------");
+        for (int i = 0; i < humans.length; i++){
+            System.out.println(humans[i].getName() + "\t " + humans[i].getAge());
+        }
+    }
+}
