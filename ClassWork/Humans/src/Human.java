ad2369e2e8cf0f1ea15c83b57ce227cbef5de1b3
Ilmir <ilmir_kashapov@mail.ru>
2017-04-04 23:35:44 +0300

    add all

--- ClassWork/Humans/src/Human.java
+++ ClassWork/Humans/src/Human.java
@@ -1,8 +1,7 @@
 /**
- * Created by Ilmir on 28.03.2017.
+ * Created by User on 25.03.2017.
  */
 public class Human {
-
     private String name;
     private int age;
 
@@ -11,19 +10,19 @@ public class Human {
         this.age = age;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getAge() {
         return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
