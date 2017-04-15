ca41981e52c35e29b204feda3ec19056078645d5
Ilmir <ilmir_kashapov@mail.ru>
2017-04-15 10:24:11 +0300

    Task21

---
+++ HomeWorks/Task21/src/StringFromChar.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Ilmir on 15.04.2017.
+ */
+public class StringFromChar {
+    public static void main(String[] args) {
+
+        char[] b;
+        b = new char[3];
+        char[] a = new char[]{'3','2','1'};
+
+
+        String str = new String(a);
+        System.out.println(str);
+
+    }
+}
