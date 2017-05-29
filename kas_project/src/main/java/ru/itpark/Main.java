128d2fdd5bcfe0953572d6a888d507caf7da3c6c
Ilmir <ilmir_kashapov@mail.ru>
2017-05-29 13:46:13 +0300

    proj

--- kas_project/src/main/java/ru/itpark/Main.java
+++ kas_project/src/main/java/ru/itpark/Main.java
@@ -5,6 +5,8 @@ import org.springframework.core.env.ConfigurableEnvironment;
 import ru.itpark.models.User;
 import ru.itpark.service.UsersService;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,15 +16,15 @@ public class Main {
         context.load("ru.itpark\\context.xml");
         context.refresh();
         UsersService usersService = context.getBean(UsersService.class);
-        //User user = new User("minimen", "177@mail");
-        //usersService.register(user);
+        User user = new User("minimen", "951542555");
+        usersService.register(user);
         User mishaFromBd = usersService.getUserById(6);
         System.out.println(mishaFromBd.getId() + " " + mishaFromBd.getName());
 
-        //List<User> users = usersService.getListOfUsers();
-        //System.out.println(users);
-       //usersService.removeUser(11);
-       // User lena = new User(7, "Lena", 30);
-      //  usersService.update(lena);
+        List<User> users = usersService.getListOfUsers();
+        System.out.println(users);
+        usersService.removeUser(11);
+        User lena = new User(7, "Lena", "951542512");
+        usersService.update(lena);
     }
 }
